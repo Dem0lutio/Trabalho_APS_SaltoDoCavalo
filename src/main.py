caaa50a74159b32e0ac9547903e7493c7c6e0db3
unknown <pedrogomesmagri@gmail.com>
2024-11-21 04:02:29 -0300

    Main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from interface.cavalo_interface import CavaloInterface
+from interface.old_cavalo_interface import OldCavaloInterface
 
 if __name__ == '__main__':
-    CavaloInterface()
+    OldCavaloInterface()
