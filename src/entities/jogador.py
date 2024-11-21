6580e692a808da0b3dc2cf820d4fcd0f66844600
unknown <pedrogomesmagri@gmail.com>
2024-11-20 23:32:40 -0300

    Adiciona entidades

---
+++ src/entities/jogador.py
@@ -0,0 +1,50 @@
+from posicao import Posicao
+
+class Jogador():
+    def __init__(self):
+        
+        self._nome = ''
+        self._cor = ''
+        self._posicao_atual = None
+        self._winner = False
+        self._turno = False
+
+    @property
+    def nome(self) -> str:
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome: str) -> None:
+        self._nome = nome
+
+    @property
+    def cor(self) -> str:
+        return self._cor
+
+    @cor.setter
+    def cor(self, cor: str) -> None:
+        self._cor = cor
+
+    @property
+    def posicao_atual(self):
+        return self._posicao_atual
+
+    @posicao_atual.setter
+    def posicao_atual(self, posicao):
+        self._posicao_atual = posicao
+
+    @property
+    def winner(self) -> bool:
+        return self._winner
+
+    @winner.setter
+    def winner(self, winner: bool) -> None:
+        self._winner = winner
+
+    @property
+    def turno(self) -> bool:
+        return self._turno
+
+    @turno.setter
+    def turno(self, turno: bool) -> None:
+        self._turno = turno
