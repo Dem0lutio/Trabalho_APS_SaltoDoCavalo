5093154dfac78955a9bd785a695812b155b03755
unknown <pedrogomesmagri@gmail.com>
2024-11-22 00:33:10 -0300

    Ajustes nas entidades e interface

--- src/entities/posicao.py
+++ src/entities/posicao.py
@@ -1,56 +1,55 @@
 class Posicao():
     def __init__(self, x: int, y: int):
-
         self._x = x
         self._y = y
         self._ocupada = ''
-        self._bloqueada = True if ((x == 4 and y == 4) or (x == 0 and y == 0)) else False
+        self._bloqueada = False
 
-        @property
-        def x(self) -> int:
-            return self._x
+    @property
+    def x(self) -> int:
+        return self._x
 
-        @x.setter
-        def x(self, x: int) -> None:
-            self._x = x
+    @x.setter
+    def x(self, x: int) -> None:
+        self._x = x
 
-        @property
-        def y(self) -> int:
-            return self._y
+    @property
+    def y(self) -> int:
+        return self._y
 
-        @y.setter
-        def y(self, y: int) -> None:
-            self._y = y
+    @y.setter
+    def y(self, y: int) -> None:
+        self._y = y
 
-        @property
-        def bloqueada(self) -> bool:
-            return self._bloqueada
+    @property
+    def bloqueada(self) -> bool:
+        return self._bloqueada
 
-        @bloqueada.setter
-        def bloqueada(self, bloqueada: bool) -> None:
-            self._bloqueada = bloqueada
-        
-        @property
-        def ocupada(self) -> bool:
-            return self._ocupada
+    @bloqueada.setter
+    def bloqueada(self, bloqueada: bool) -> None:
+        self._bloqueada = bloqueada
+    
+    @property
+    def ocupada(self) -> bool:
+        return self._ocupada
 
-        @ocupada.setter
-        def ocupada(self, ocupada: bool) -> None:
-            self._bloqueada = ocupada
+    @ocupada.setter
+    def ocupada(self, ocupada: bool) -> None:
+        self._ocupada = ocupada
 
-        def posicao_alcancavel(self, posicao_atual: Posicao, posicao_destino: Posicao) -> bool:
-            """
-                Retorna se é possível o jogador se movimentar para a posição selecionada.
+    def posicao_alcancavel(self, posicao_atual, posicao_destino) -> bool:
+        """
+            Retorna se é possível o jogador se movimentar para a posição selecionada.
 
-                :param posicao_atual: Posição atual do jogador no tabuleiro.
-                :param posicao_destino: Posição destino selecionada pelo jogador.
-                :return: Booleano indicando se a posição é alcançável.
-            """
+            :param posicao_atual: Posição atual do jogador no tabuleiro.
+            :param posicao_destino: Posição destino selecionada pelo jogador.
+            :return: Booleano indicando se a posição é alcançável.
+        """
 
-            dx = abs(posicao_atual.x - posicao_destino.x)
-            dy = abs(posicao_atual.y - posicao_destino.y)
+        dx = abs(posicao_atual.x - posicao_destino.x)
+        dy = abs(posicao_atual.y - posicao_destino.y)
 
-            if posicao_destino.bloqueada:
-                return False
+        if posicao_destino.bloqueada:
+            return False
 
-            return (dx == 2 and dy == 1) or (dx == 1 and dy == 2)
+        return (dx == 2 and dy == 1) or (dx == 1 and dy == 2)
--- src/entities/tabuleiro.py
+++ src/entities/tabuleiro.py
@@ -5,11 +5,17 @@ from entities.posicao import Posicao
 
 class Tabuleiro():
     def __init__(self, interface):
-        self._status_partida = 'PARTIDA NAO INICIADA'
         self._posicoes = []
+        self._status_partida = 'PARTIDA NAO INICIADA'
         self._interface = interface
         self._jogador_local = Jogador(self)
         self._jogador_remoto = Jogador(self)
+
+        for linha in range(5):
+            coluna_posicoes = []
+            for coluna in range(5):
+                coluna_posicoes.append(Posicao(linha, coluna))
+            self._posicoes.append(coluna_posicoes)
 
     @property
     def posicoes(self):
@@ -24,41 +30,47 @@ class Tabuleiro():
         self._status_partida = status_partida
     
     def iniciar_partida(self, jogadores: list[str]) -> None:
-        self._jogador_local.nome(jogadores[0][0])
-        self._jogador_remoto.nome(jogadores[1][0])
+        self._jogador_local.nome = jogadores[0][0]
+        self._jogador_remoto.nome = jogadores[1][0]
         
-        if jogadores[0][2] == "1":
-            self._jogador_local.cor('BRANCO')
-            self._jogador_remoto.cor('PRETO')
+        if jogadores[0][2] == '1':
+            self._jogador_local.cor = 'BRANCO'
+            self._jogador_remoto.cor = 'PRETO'
             self._jogador_local.trocar_turno()
             self.status_partida = 'AGUARDANDO SELECAO DO CAVALO'
         else:
-            self._jogador_local.cor('PRETO')
-            self._jogador_remoto.cor('BRANCO')
+            self._jogador_local.cor = 'PRETO'
+            self._jogador_remoto.cor = 'BRANCO'
             self._jogador_remoto.trocar_turno()
             self.status_partida = 'AGUARDANDO JOGADOR REMOTO'
 
-        self.posicoes.posicoes_iniciais()
+        self.posicoes_iniciais()
 
     def posicoes_iniciais(self):
-        self.posicoes[4][4].bloqueada(True)
-        self.posicoes[4][4].ocupada('CAVALO BRANCO')
-        self.posicoes[0][0].bloqueada(True)
-        self.posicoes[4][4].ocupada('CAVALO PRETO')
+
+        for linha in range(5):
+            for coluna in range(5):
+                self._posicoes[linha][coluna].bloqueada = False
+                self._posicoes[linha][coluna].ocupada = ''
+
+        self._posicoes[4][4].bloqueada = True
+        self._posicoes[4][4].ocupada = 'CAVALO BRANCO'
+        self._posicoes[0][0].bloqueada = True
+        self._posicoes[0][0].ocupada = 'CAVALO PRETO'
 
         if self._jogador_local.cor == 'BRANCO':
-            self._jogador_local.posicao_atual(self.posicoes[4][4])
-            self._jogador_remoto.posicao_atual(self.posicoes[0][0])
+            self._jogador_local.posicao_atual = self._posicoes[4][4]
+            self._jogador_remoto.posicao_atual = self._posicoes[0][0]
         else:
-            self._jogador_local.posicao_atual(self.posicoes[0][0])
-            self._jogador_remoto.posicao_atual(self.posicoes[4][4])
+            self._jogador_local.posicao_atual = self._posicoes[0][0]
+            self._jogador_remoto.posicao_atual = self._posicoes[4][4]
 
     def selecionar_posicao(self, linha: int, coluna: int) -> Union[Dict, None]:
-        status_partida = self.status_partida()
+        status_partida = self.status_partida
         
-        posicao_selecionada = self.posicoes[linha, coluna]
+        posicao_selecionada = self.posicoes[linha][coluna]
 
-        if status_partida == 'AGUARDANDO SELECAO CAVALO':
+        if status_partida == 'AGUARDANDO SELECAO DO CAVALO':
             return self.selecionar_posicao_origem(posicao_selecionada)
         elif status_partida == 'AGUARDANDO SELECAO DESTINO':
             return self.selecionar_posicao_destino(posicao_selecionada)
@@ -67,11 +79,11 @@ class Tabuleiro():
         jogador_atual = self.get_jogador_atual()
         
         if posicao_selecionada == jogador_atual.posicao_atual:
-            self.status_partida('AGUARDANDO SELECAO DESTINO')
+            self.status_partida = 'AGUARDANDO SELECAO DESTINO'
             if jogador_atual.cor == 'BRANCO':
-                posicao_selecionada.ocupada('CAVALO BRANCO SELECIONADO')
+                posicao_selecionada.ocupada = 'CAVALO BRANCO SELECIONADO'
             else:
-                posicao_selecionada.ocupada('CAVALO PRETO SELECIONADO')
+                posicao_selecionada.ocupada = 'CAVALO PRETO SELECIONADO'
         else:
             self._interface.notificacao('Você deve selecionar o seu respectivo cavalo!')
 
@@ -83,7 +95,7 @@ class Tabuleiro():
 
         if not self.posicao_bloqueada(posicao_destino.x, posicao_destino.y):
 
-            if posicao_destino.posicao_alcancavel(posicao_origem):
+            if posicao_destino.posicao_alcancavel(posicao_origem, posicao_destino):
                 jogada = {'linha_origem': posicao_origem.x,
                           'coluna_origem': posicao_origem.y,
                           'linha_destino': posicao_destino.x,
@@ -91,11 +103,12 @@ class Tabuleiro():
                           'tipo': 'jogada'}
                 
                 if jogador_atual.cor == 'BRANCO':
-                    posicao_destino.ocupada('CAVALO BRANCO')
-                    posicao_destino.bloqueada(True)
+                    posicao_destino.ocupada = 'CAVALO BRANCO'
+                    posicao_destino.bloqueada = True
                 else:
-                    posicao_destino.ocupada('CAVALO PRETO')
-                    posicao_destino.bloqueada(True)
+                    posicao_destino.ocupada = 'CAVALO PRETO'
+                    posicao_destino.bloqueada = True
+                posicao_origem.ocupada = ''
                 jogador_atual.posicao_atual = posicao_destino
 
                 self._jogador_local.trocar_turno()
@@ -108,7 +121,7 @@ class Tabuleiro():
                     jogada['match_status'] = 'finished'
                 else:
                     if jogador_atual == self._jogador_local:
-                        self.status_partida = 'AGUARDANDO SELECAO CAVALO'
+                        self.status_partida = 'AGUARDANDO SELECAO DO CAVALO'
                     else:
                         self.status_partida = 'AGUARDANDO JOGADOR REMOTO'
                     jogada['match_status'] = 'next'
@@ -120,8 +133,8 @@ class Tabuleiro():
 
     def receber_jogada(self, jogada: Dict) -> None:
         if jogada['tipo'] == "jogada":
-            self.selecionar_posicao_origem(jogada["linha_origem"], jogada["coluna_origem"])
-            self.selecionar_posicao_destino(jogada["linha_destino"], jogada["coluna_destino"])
+            self.selecionar_posicao_origem(self.posicoes[jogada['linha_origem']][jogada['coluna_origem']])
+            self.selecionar_posicao_destino(self.posicoes[jogada['linha_destino']][jogada['coluna_destino']])
 
     def verifica_vencedor(self):
         if self._jogador_local.turno:
@@ -129,8 +142,11 @@ class Tabuleiro():
         else:
             return True if not self._jogador_remoto.movimentos_possiveis() else False
             
-    def posicao_bloqueada(self, linha: int, coluna: int):
-        return self.posicoes[linha, coluna].bloqueada
+    def posicao_bloqueada(self, linha: int, coluna: int) -> bool:
+        return self._posicoes[linha][coluna].bloqueada
+    
+    def posicao_ocupada(self, linha: int, coluna: int) -> str:
+        return self._posicoes[linha][coluna].ocupada
     
     def get_jogador_atual(self) -> Jogador:
         return self._jogador_local if self._jogador_local.turno else self._jogador_remoto
